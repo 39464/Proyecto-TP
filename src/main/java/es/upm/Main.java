7876aee9b932e58706e7c59aae00404059470eb7
bw0038 <i.lombardo@alumnos.upm.es>
2025-11-17 21:59:44 +0100

    Clase main empezada (solo puse el menú, literalmente nada importante)

--- src/main/java/es/upm/Main.java
+++ src/main/java/es/upm/Main.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Por hacer: Implementar la lógica principal de la aplicación aquí
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("---Menú principal---\n");
+        System.out.println("1. Agregar Actividad\n " +
+                "2. Consultar/Editar Actividad\n" +
+                "3. Guardar Actividades\n"+
+                "4. Cargar Actividades\n" +
+                "5. Planificar Viaje\n" +
+                "6. Guardar Itinerario\n" +
+                "7. Salir\n");
+        int opcion = teclado.nextInt();
+        // @TODO: Implementar la lógica principal de la aplicación aquí
     }
 }
