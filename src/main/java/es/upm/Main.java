a7d3e45ed1e677671011371a8255bbdd22bd171b
bw0038 <i.lombardo@alumnos.upm.es>
2025-12-15 20:03:44 +0100

    Main hecho ;)

--- src/main/java/es/upm/Main.java
+++ src/main/java/es/upm/Main.java
@@ -1,18 +1,44 @@
 package es.upm;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        if(args.length < 4 || args.length > 6){
-            System.out.println("Número de argumentos incorrecto.\n" +
-                    "Uso correcto: <maxRecursosPorActividad>, <maxComentariosPorActividad>, <maxActividadesEnCatalogo>" +
-                    ", <numDiasViaje>, <maxActividadesPorDia>, <nombreArchivoActividades (opcional)>");
+        try {
+            if (args.length < 5 || args.length > 6) {
+                System.out.println("Número de argumentos incorrecto.\n" +
+                        "Uso correcto: <maxRecursosPorActividad>, <maxComentariosPorActividad>, <maxActividadesEnCatalogo>" +
+                        ", <numDiasViaje>, <maxActividadesPorDia>, <nombreArchivoActividades (opcional)>");
+            }
+            Scanner teclado = new Scanner(System.in);
+            int maxRecActividad = Integer.parseInt(args[0]);
+            int maxComActividad = Integer.parseInt(args[1]);
+            int maxActCatalogo = Integer.parseInt(args[2]);
+            int numDiasViaje = Integer.parseInt(args[3]);
+            int maxActDia = Integer.parseInt(args[4]);
+            String nombreArchivo = null;
+
+            CatalogoActividades catalogo = new CatalogoActividades(maxActCatalogo);
+            Viaje viaje = new Viaje(numDiasViaje, maxActDia);
+            InterfazUsuario interfaz = new InterfazUsuario(catalogo, viaje, maxRecActividad, maxComActividad);
+
+            if (args.length == 6) {
+                nombreArchivo = args[5];
+            }
+            if (nombreArchivo != null) {
+                try {
+                    catalogo.cargarActividades(nombreArchivo, maxRecActividad, maxComActividad);
+                    System.out.println("Actividades cargadas exitosamente desde el archivo");
+                } catch (Exception e) {
+                    System.out.println("Error al cargar las actividades: " + e.getMessage());
+                }
+            }
+            interfaz.iniciar(teclado);
+        } catch (NumberFormatException ex){
+            System.out.println("Los números proporcionados como argumento deben ser enteros");
+        } catch (Exception e){
+            System.out.println("Error al ejecutar: " + e.getMessage());
         }
-        Scanner teclado = new Scanner(System.in);
-        CatalogoActividades catalogo = new CatalogoActividades(Integer.parseInt(args[2]));
-        Viaje viaje = new Viaje(Integer.parseInt(args[3]), Integer.parseInt(args[4]));
-        InterfazUsuario interfaz = new InterfazUsuario(catalogo, viaje, Integer.parseInt(args[0]), Integer.parseInt(args[1]));;
-        interfaz.iniciar(teclado);
     }
 }
